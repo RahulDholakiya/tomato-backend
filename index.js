ee6c5f95bbeeda6931ab19690c3ba64fefb57b3d
rahul dholakiya <dholakiyarahul0@gmail.com>
2024-05-13 18:12:10 +0530

    create some router

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRoute from "./routes/foodRoute.js";
 
 // app config
 const app = express();
@@ -8,6 +10,12 @@ const port = 4000;
 // middleware
 app.use(express.json());
 app.use(cors());
+
+//db connection
+connectDB();
+
+// api endpoint
+app.use("/api/food", foodRoute);
 
 app.get("/", (req, res) => {
   res.send("Api Working");
